7d691b92148e4131361fee39edf7d48ecaa322c0
aluno <anthonysheridanbarbaro@gmail.com>
2020-02-14 08:20:07 -0300

    fiz oq o professor pediu...

--- teste.c
+++ teste.c
@@ -3,6 +3,15 @@
 
 int main(){
 
+    int idade, i;
+
+    printf("Informe sua idade: ");
+    scanf("%i", &idade);
+
+    i = idade - 1;
+
+    printf("Sua idade menos 1 é: %i\n\n", i);
+
 
     return 0;
 }
